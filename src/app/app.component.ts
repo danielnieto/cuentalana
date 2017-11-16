cdc2d6cba44d4002285800098f68cd84371cdcfe
Daniel Nieto <danielnieto89@gmail.com>
2017-11-16 16:05:42 -0600

    select text of inputs when focused, use placeholder to show initial quantity and make the inputs to be [type=number]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,25 +8,25 @@ import { Denomination } from './denomination';
 export class AppComponent {
 
     bills: Array < Denomination > = [
-        new Denomination(1000, 0),
-        new Denomination(500, 0),
-        new Denomination(200, 0),
-        new Denomination(100, 0),
-        new Denomination(50, 0),
-        new Denomination(20, 0)
+        new Denomination(1000, null),
+        new Denomination(500, null),
+        new Denomination(200, null),
+        new Denomination(100, null),
+        new Denomination(50, null),
+        new Denomination(20, null)
     ];
 
     coins: Array < Denomination > = [
-        new Denomination(50, 0),
-        new Denomination(20, 0),
-        new Denomination(10, 0),
-        new Denomination(5, 0),
-        new Denomination(2, 0),
-        new Denomination(1, 0),
-        new Denomination(0.5, 0),
-        new Denomination(0.2, 0),
-        new Denomination(0.1, 0),
-        new Denomination(0.05, 0)
+        new Denomination(50, null),
+        new Denomination(20, null),
+        new Denomination(10, null),
+        new Denomination(5, null),
+        new Denomination(2, null),
+        new Denomination(1, null),
+        new Denomination(0.5, null),
+        new Denomination(0.2, null),
+        new Denomination(0.1, null),
+        new Denomination(0.05, null)
     ];
 
 
