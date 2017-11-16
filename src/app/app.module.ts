d1c201e99d2033b9e9050c7b55d15cc44c85dead
Daniel Nieto <danielnieto89@gmail.com>
2017-11-16 15:47:38 -0600

    remove all @angular/material and @angular/cdk packages and references
    as well as reset.css and material theme imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule} from '@angular/material';
 import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
